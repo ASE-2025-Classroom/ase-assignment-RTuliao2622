0e2768069c8b732017af06126fb53918c8bc8811
Robert Tuliao <R.Tuliao2622@student.leedsbeckett.ac.uk>
2025-10-21 17:40:35 +0100

    Properly Installed BOOSE

---
+++ BOOSEapp/Form1.cs
@@ -0,0 +1,14 @@
+using BOOSE;
+using System.Diagnostics;
+
+namespace BOOSEapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
+        }
+    }
+}
