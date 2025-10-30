6ffd9cba73cf7b1d9375c4c11ac70cf359a27141
Robert Tuliao <R.Tuliao2622@student.leedsbeckett.ac.uk>
2025-10-30 16:23:01 +0000

    Form2 - Runs boose commands entered in textbox + basic validation

---
+++ BOOSEapp/Form2.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BOOSEapp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            canvasBox = new PictureBox();
+            runButton = new Button();
+            textBox = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)canvasBox).BeginInit();
+            SuspendLayout();
+            // 
+            // canvasBox
+            // 
+            canvasBox.Location = new Point(398, 22);
+            canvasBox.Name = "canvasBox";
+            canvasBox.Size = new Size(378, 399);
+            canvasBox.TabIndex = 0;
+            canvasBox.TabStop = false;
+            // 
+            // runButton
+            // 
+            runButton.Location = new Point(130, 383);
+            runButton.Name = "runButton";
+            runButton.Size = new Size(113, 38);
+            runButton.TabIndex = 1;
+            runButton.Text = "RUN";
+            runButton.UseVisualStyleBackColor = true;
+            runButton.Click += runButton_Click;
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(25, 22);
+            textBox.Multiline = true;
+            textBox.Name = "textBox";
+            textBox.Size = new Size(345, 339);
+            textBox.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox);
+            Controls.Add(runButton);
+            Controls.Add(canvasBox);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)canvasBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox canvasBox;
+        private Button runButton;
+        private TextBox textBox;
+    }
+}
---
+++ BOOSEapp/Form2.cs
@@ -0,0 +1,52 @@
+﻿using BOOSE;
+using BOOSEApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BOOSEapp
+{
+    public partial class Form2 : Form
+    {
+        new AppCanvas canvas;
+        ///<summary>
+        /// Initializes the form and sets up the drawing canvas.
+        /// </summary>
+        public Form2()
+        {
+            InitializeComponent();
+
+            canvas = new AppCanvas(canvasBox.Width, canvasBox.Height);
+            canvasBox.Image = (Bitmap)canvas.getBitmap();
+        }
+        /// <summary>
+        /// Click event for the run button. Allows user to input valid boose commands which is then outputted onto the picture box.
+        /// </summary>
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string commands = textBox.Text;
+
+                var program = new StoredProgram(canvas);
+                var factory = new CommandFactory();
+                var parser = new Parser(factory, program);
+
+                parser.ParseProgram(commands);
+                program.Run();
+
+                canvasBox.Image = (Bitmap)canvas.getBitmap();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
